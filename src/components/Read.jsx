ec40e1f143bd44cc5d365e4dcd61ce100cc317e0
Elansezhian Rajabadar <119504018+rsezhian@users.noreply.github.com>
2024-06-18 14:33:43 +0530

    Update Read.jsx

--- src/components/Read.jsx
+++ src/components/Read.jsx
@@ -53,7 +53,7 @@ const Read = () => {
       <div className="row">
         {data?.map((item) => (
           <div key={item._id} className="col-3">
-            <div className="card">
+            <div className="card mb-3">
               <div className="card-body">
                 <h5 className="card-title">{item.name}</h5>
                 <h6 className="card-subtitle mb-2 text-body-secondary">
